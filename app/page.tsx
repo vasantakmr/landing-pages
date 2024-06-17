cd83bdc8831f990ae20a32c4044809ecf24ea6ef
Vasanta Kumar <vasantakmr6@gmail.com>
2024-06-18 05:27:59 +0530

    added new dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { IDE } from "./ide";
+import { Dashboard } from "./dashboard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-8 py-24">
-      <IDE/>
+      <Dashboard/>
     </main>
   );
 }
