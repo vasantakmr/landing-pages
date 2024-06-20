f6fdb6d47333623bca6699b98749ada735968593
Vasanta Kumar <vasantakmr6@gmail.com>
2024-06-20 05:47:39 +0530

    added command search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { CircleUser, Package2, Search, Menu } from "lucide-react";
+import { CircleUser, Package2, Menu } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -9,7 +9,6 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -20,6 +19,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ModeToggle } from "./ModeToggle";
 import { Links } from "./Links";
+import { SearchCommandDialog } from "./SearchCommandDialog";
 
 export function Navbar() {
   return (
@@ -39,7 +39,7 @@ export function Navbar() {
               className="flex items-center gap-2 text-lg font-semibold"
             >
               <Package2 className="h-6 w-6" />
-              <span className="sr-only">Acme Inc</span>
+              <span className="sr-only">Gurucodes Inc</span>
             </Link>
             <Link href="/" className="hover:text-foreground">
               Home
@@ -67,14 +67,7 @@ export function Navbar() {
       </Sheet>
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
         <form className="ml-auto flex-1 sm:flex-initial">
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search products..."
-              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-            />
-          </div>
+          <SearchCommandDialog />
         </form>
         <ModeToggle />
         <DropdownMenu>
---
+++ components/SearchCommandDialog.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import * as React from "react";
+import {
+  BookCopy,
+  BriefcaseBusiness,
+  Calculator,
+  CreditCard,
+  Settings,
+  User,
+} from "lucide-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export function SearchCommandDialog() {
+  const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "/" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <div
+        className="relative"
+        onClick={(e) => {
+          e.preventDefault();
+          setOpen(true);
+        }}
+      >
+        <Button
+          variant="outline"
+          className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+        >
+          <span className="text-sm text-muted-foreground absolute left-2.5 top-2.5 h-4  text-muted-foreground">
+            Press{" "}
+            <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+              <span className="text-xs">⌘</span>/
+            </kbd>
+            to search
+          </span>
+        </Button>
+      </div>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <Link href="/learn">
+              <CommandItem onSelect={() => setOpen(false)}>
+                <BookCopy className="mr-2 h-4 w-4" />
+                <span>Learn</span>
+              </CommandItem>
+            </Link>
+            <Link href="/jobs">
+              <CommandItem onSelect={() => setOpen(false)}>
+                <BriefcaseBusiness className="mr-2 h-4 w-4" />
+                <span>Jobs</span>
+              </CommandItem>
+            </Link>
+            <CommandItem onSelect={() => setOpen(false)}>
+              <Calculator className="mr-2 h-4 w-4" />
+              <span>S</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
