8d5f03708fe556cbae6139d38996eb223ff21806
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-06-22 05:46:44 +0530

    learn page done

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} transition-all duration-1000`}>
+      <body className={`${inter.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
