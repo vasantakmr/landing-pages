c4fdf5d10b32bbf26d3736a2f3ef3b82bc4dde4e
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-07-09 01:47:00 +0530

    modules height updated

--- components/landingpage/courseoffering/ModulesList.tsx
+++ components/landingpage/courseoffering/ModulesList.tsx
@@ -14,14 +14,14 @@ const ModulesList = () => {
     <Carousel>
       <CarouselNext className="top-1/3 -translate-y-1/3" />
       <CarouselPrevious className="top-1/3 -translate-y-1/3" />
-      <CarouselMainContainer className="h-60">
-        {Array.from({ length: 37 }).map((_, index) => (
+      <CarouselMainContainer className="h-80">
+        {Array.from({ length: 36 }).map((_, index) => (
           <SliderMainItem key={index} className="bg-transparent">
             <div className="outline outline-1 outline-border size-full flex items-center justify-center rounded-xl bg-background">
               <Image
                 src={`/modules/${index + 1}.jpg`}
-                width={300}
-                height={100}
+                width={450}
+                height={400}
                 alt={`module.no${index + 1}`}
               ></Image>
             </div>
@@ -29,7 +29,7 @@ const ModulesList = () => {
         ))}
       </CarouselMainContainer>
       <CarouselThumbsContainer>
-        {Array.from({ length: 37 }).map((_, index) => (
+        {Array.from({ length: 36 }).map((_, index) => (
           <SliderThumbItem key={index} index={index} className="bg-transparent">
             <div className="outline outline-1 outline-border size-full flex items-center justify-center rounded-xl bg-background">
               Module {index + 1}
