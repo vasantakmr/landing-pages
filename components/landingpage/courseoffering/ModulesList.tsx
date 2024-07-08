a3db8d2f0d9234180a37423b0ad5e0a4161110f8
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2024-07-09 01:26:17 +0530

    modules added

--- components/landingpage/courseoffering/ModulesList.tsx
+++ components/landingpage/courseoffering/ModulesList.tsx
@@ -1,39 +1,44 @@
 import {
-    Carousel,
-    CarouselMainContainer,
-    CarouselNext,
-    CarouselPrevious,
-    SliderMainItem,
-    CarouselThumbsContainer,
-    SliderThumbItem,
-  } from "@/components/extension/carousel";
-  
-  const ModulesList = () => {
-    return (
-      <Carousel>
-        <CarouselNext className="top-1/3 -translate-y-1/3" />
-        <CarouselPrevious className="top-1/3 -translate-y-1/3" />
-        <CarouselMainContainer className="h-60">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <SliderMainItem key={index} className="bg-transparent">
-              <div className="outline outline-1 outline-border size-full flex items-center justify-center rounded-xl bg-background">
-                Slide {index + 1}
-              </div>
-            </SliderMainItem>
-          ))}
-        </CarouselMainContainer>
-        <CarouselThumbsContainer>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <SliderThumbItem key={index} index={index} className="bg-transparent">
-              <div className="outline outline-1 outline-border size-full flex items-center justify-center rounded-xl bg-background">
-                Slide {index + 1}
-              </div>{" "}
-            </SliderThumbItem>
-          ))}
-        </CarouselThumbsContainer>
-      </Carousel>
-    );
-  };
-  
-  export default ModulesList;
-  
+  Carousel,
+  CarouselMainContainer,
+  CarouselNext,
+  CarouselPrevious,
+  SliderMainItem,
+  CarouselThumbsContainer,
+  SliderThumbItem,
+} from "@/components/extension/carousel";
+import Image from "next/image";
+
+const ModulesList = () => {
+  return (
+    <Carousel>
+      <CarouselNext className="top-1/3 -translate-y-1/3" />
+      <CarouselPrevious className="top-1/3 -translate-y-1/3" />
+      <CarouselMainContainer className="h-60">
+        {Array.from({ length: 37 }).map((_, index) => (
+          <SliderMainItem key={index} className="bg-transparent">
+            <div className="outline outline-1 outline-border size-full flex items-center justify-center rounded-xl bg-background">
+              <Image
+                src={`/modules/${index + 1}.jpg`}
+                width={300}
+                height={100}
+                alt={`module.no${index + 1}`}
+              ></Image>
+            </div>
+          </SliderMainItem>
+        ))}
+      </CarouselMainContainer>
+      <CarouselThumbsContainer>
+        {Array.from({ length: 37 }).map((_, index) => (
+          <SliderThumbItem key={index} index={index} className="bg-transparent">
+            <div className="outline outline-1 outline-border size-full flex items-center justify-center rounded-xl bg-background">
+              Module {index + 1}
+            </div>{" "}
+          </SliderThumbItem>
+        ))}
+      </CarouselThumbsContainer>
+    </Carousel>
+  );
+};
+
+export default ModulesList;
